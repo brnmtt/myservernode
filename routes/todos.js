e75bbb91e536e4ee10ca5706549365489d25bf06
brnmtt <bernardini.mattia@gmail.com>
2019-05-17 09:37:27 +0200

    punto 2 es4

--- routes/todos.js
+++ routes/todos.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const list = require('../data/lista')
 
 router.get('/', (req, res) => {
-    res.send(list)
-  })
+  res.send(list)
+})
+
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  
+  res.send(list.filter(item => item.id === id))
+})
 
   module.exports = router
